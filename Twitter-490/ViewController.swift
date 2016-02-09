00e7fd4e9635697f70b0116439995b0563e33273
akshatgyl <akshatgoyal@akshats-macbook-pro.local>
2016-02-08 22:02:58 -0500

    Initial Commit

---
+++ Twitter-490/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter-490
+//
+//  Created by Akshat Goyal on 2/8/16.
+//  Copyright © 2016 akshat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
